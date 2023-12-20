afc833fb7675e9f5345ddf6e45756e7ef1a5d70a
Jim Torre <jim.torre@bytecode.io>
2023-12-20 09:06:54 -0500

    update config

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,18 +1,13 @@
 let path = require('path');
 
 let webpackConfig = {
-    mode: 'development',
+    mode: 'production',
     entry: {
         single_icon: './src/visualizations/single_icon.js'
     },
     output: {
         filename: '[name].js',
         path: path.join(__dirname, 'dist'),
-        library: '[name]',
-        libraryTarget: 'umd'
-    },
-    resolve: {
-        extensions: ['.ts', '.js', '.scss', '.css']
     },
     module: {
         rules: [
@@ -21,12 +16,6 @@ let webpackConfig = {
             { test: /\.(woff|woff2)$/, use:  'url-loader'},
         ],
     },
-    devServer: {
-        static: "./dist",
-        port: 8080,
-        https: true,
-    },
-    devtool: 'source-map',
 };
 
 module.exports = webpackConfig;
